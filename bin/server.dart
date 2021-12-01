3bce1efcfe8d6dff375002a98bc605be47fb418a
DocKoolaid <w0299737@nscc.ca>
2021-11-30 20:07:15 -0400

    Refactored and made everything work :D

--- bin/server.dart
+++ bin/server.dart
@@ -1,10 +1,9 @@
 import 'dart:io';
 
-import 'package:postgres/postgres.dart';
 import 'package:postgres_pool/postgres_pool.dart';
 import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart';
-import 'todo_service.dart';
+import 'services/todo_service.dart';
 
 void main(List<String> args) async {
   // Use any available host or container IP (usually `0.0.0.0`).
@@ -12,20 +11,14 @@ void main(List<String> args) async {
   // Configure a pipeline that logs requests.
 
   PgPool _postgres_pool = PgPool(
-      PgEndpoint(
-          host: "127.0.0.1",
-          port: 5432,
-          username: "dev",
-          database: "todo",
-          password: "dev"),
+      PgEndpoint(host: "127.0.0.1", port: 5432, username: "dev", database: "todos", password: "dev"),
       settings: PgPoolSettings()..concurrency = 6);
 
   final _todoService = TodoService(_postgres_pool);
-  final _handler =
-      Pipeline().addMiddleware(logRequests()).addHandler(_todoService.router);
+  final _handler = Pipeline().addMiddleware(logRequests()).addHandler(_todoService.router);
 
   // For running in containers, we respect the PORT environment variable.
-  final port = int.parse(Platform.environment['PORT'] ?? '8080');
+  final port = int.parse(Platform.environment['PORT'] ?? '42069');
   final server = await serve(_handler, ip, port);
   print('Server listening on port ${server.port}');
 }
--- bin/services/todo_service.dart
+++ bin/services/todo_service.dart
@@ -1,21 +1,23 @@
-import 'package:postgresql2/postgresql.dart';
+import 'package:postgres_pool/postgres_pool.dart';
 import 'package:shelf/shelf.dart';
 import 'package:shelf_router/shelf_router.dart';
 
-import 'todo_repo.dart';
+import '../repos/todo_repo.dart';
 
 part 'todo_service.g.dart';
 
 class TodoService {
   TodoRepo? _repo;
 
-  TodoService(Connection connection) {
+  TodoService(PgPool connection) {
     _repo = TodoRepo(connection);
   }
 
   @Route.get("/todos")
   Future<Response> showAllTodos(Request request) async {
-    return Response.ok(await _repo!.getAllTodos());
+
+
+    return Response.ok((await _repo!.getAllTodos()).toString(), headers: {"Content-type":"application/json"});
   }
 
   Router get router => _$TodoServiceRouter(this);
